4111f17902f206dbd96c15ee8bf65aff6d91da2e
tworns <tworns@yahoo.com>
2016-06-21 11:23:40 -0400

    Added text added listener

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 var express = require('express');
 var firebase = require('firebase');
 var http = require('http');
-
+var dotenv = require('dotenv');
 var app = express();
 var server = http.createServer(app);
-server.listen(3030,function(){
-  console.log("Connected on port 3030");
-});
 
+
+//Firebase auth
   firebase.initializeApp({
   serviceAccount: "firebase-credentials.json",
   databaseURL: "https://mutantschool-e3944.firebaseio.com"
+});
+//get firebase ref
+var refRoot = firebase.database().ref();
+//listen for texts
+var textRef = refRoot.child('texts');
+textRef.on('child_added',function(snapshot){
+  console.log(snapshot.val());
+});
+
+server.listen(3030,function(){
+  console.log("Connected on port 3030");
 });
